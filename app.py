71ac912df7bba1c21f10b2ac3649f75bb3b42432
rykow <rykow123@gmail.com>
2024-02-17 13:00:24 -0600

    Starting again

---
+++ app.py
@@ -0,0 +1,85 @@
+from dotenv import load_dotenv
+import os
+import base64
+from requests import post, get
+import json
+from flask import Flask, render_template, request, jsonify
+import cgi
+form = cgi.FieldStorage()
+searchterm = form.getvalue('searchbox')
+print(searchterm)
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+    variable = request.form['variable']
+    return variable
+
+
+load_dotenv()
+
+client_id = os.getenv("CLIENT_ID")
+client_secret = os.getenv("CLIENT_SECRET")
+
+
+def get_token():
+    auth_string = client_id + ":" + client_secret
+    auth_bytes = auth_string.encode("utf-8")
+    auth_base64 = str(base64.b64encode(auth_bytes), "utf-8")
+
+    url = "https://accounts.spotify.com/api/token"
+    headers = {
+        "Authorization": "Basic " + auth_base64,
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+    data = {"grant_type": "client_credentials"}
+    result = post(url, headers=headers, data=data)
+    json_result = json.loads(result.content)
+    token = json_result["access_token"]
+    return token
+
+def get_auth_header(token):
+    return {"Authorization": "Bearer "  + token}
+
+def search_for_album(token, album_name):
+    url = "https://api.spotify.com/v1/search"
+    headers = get_auth_header(token)
+    query = f"?q={album_name}&type=album&limit=1"
+
+    query_url = url + query
+    result = get(query_url, headers=headers)
+    json_result = json.loads(result.content)["albums"]["items"]
+    if len(json_result) == 0:
+        print("No artist with this name exists...")
+        return None
+    return json_result[0]
+
+def get_album(token, album_id):
+    url = f"https://api.spotify.com/v1/albums/{album_id}"
+    headers = get_auth_header(token)
+    result = get(url, headers=headers)
+    json_result = json.loads(result.content)
+    return json_result
+
+
+@app.route('/album', methods=["POST"])
+def POST_album():
+    token = get_token()
+    result = search_for_album(token, "The New Abnormal")
+    album_id = result["id"]
+    album_info = get_album(token, album_id)
+    print(album_info)
+
+    
+
+# token = get_token()
+# result = search_for_album(token, "The New Abnormal")
+# album_id = result["id"]
+# album_info = get_album(token, album_id)
+
+# print(album_info)
